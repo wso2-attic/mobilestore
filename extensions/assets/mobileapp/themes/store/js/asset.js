27ddd56486b44f6ebfff55d6f4eaeb041be4404b
Chatura Dilan <dilan@dilan.me>
2013-10-24 20:29:28 +0530

    added install

--- extensions/assets/mobileapp/themes/store/js/asset.js
+++ extensions/assets/mobileapp/themes/store/js/asset.js
@@ -167,9 +167,24 @@ $(function () {
             asset.process(elem.data('type'), elem.data('aid'), location.href);
         }*/
        
-       appToInstall = $(this).data("app");	
+       var device = getURLParameter("device");	
+		appToInstall = $(this).data("app");
+	   devicePlatform = $(this).data("platform").toLowerCase();
+	
 		
+		$(".device-image-block-modal").each(function(index) {	
+			var platform = $(this).data("platform").toLowerCase();
+			if(devicePlatform != platform){
+				$(this).css("visibility", "hidden");
+			}
+		
+		});
+		
+		if(!(device > 0)){
 			$('#devicesList').modal('show');
+		}else{			
+			performInstalltion(device, appToInstall);
+		}
 		
        
        
