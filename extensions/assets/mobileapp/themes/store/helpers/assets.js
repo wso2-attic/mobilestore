f879b16888b297a0f93506043e04476fd895ccbd
Chatura Dilan <dilan@dilan.me>
2013-11-07 12:39:20 +0530

    store UI issues fixed

--- extensions/assets/mobileapp/themes/store/helpers/assets.js
+++ extensions/assets/mobileapp/themes/store/helpers/assets.js
@@ -16,7 +16,7 @@ var fn = that.resources;
 
 var resources = function (page, meta) {
     var o = fn(page, meta);
-    o.js.push('mobileapp-install.js');
+    o.js.push('devices.js');
     o.css.push('mobileapp-custom.css');
     return o;
 };
